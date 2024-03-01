d2038237bf3a389b79ec3a271130091370f1f0a3
paulatalim <paulactalim@gmail.com>
2024-03-01 17:29:09 -0300

    debug

--- js/sketch_home.js
+++ js/sketch_home.js
@@ -21,14 +21,14 @@ function habilitar_btn_enviar () {
 
 function preencher_info () {
     const urlParams = new URLSearchParams(location.search);
-    let nome = urlParams.get('name');
+    let nome = urlParams.get('name').replace("%20", " ");
     let email_cripto = urlParams.get('e');
 
     let email = decrypt(email_cripto);
 
     document.getElementById("title-name").innerHTML = nome;
     document.getElementById("email").value = email;
-    document.getElementById("redirectTo").value = `https://paulatalim.github.io/pesquisa-social/pages/finalizar_pesquisa.html?name=${nome}&e=${email}`;
+    document.getElementById("redirectTo").value = `https://paulatalim.github.io/pesquisa-social/pages/finalizar_pesquisa.html?name=${nome}&e=${email_cripto}`;
 }
 
 onload = () => {
