65e2fd3d4e00a6f702194dcbe93fdd9dc1d23c5d
paulatalim <paulactalim@gmail.com>
2024-03-01 15:52:20 -0300

    pagina funcional

--- js/sketch_finalizar.js
+++ js/sketch_finalizar.js
@@ -1,4 +1,21 @@
 function copiar_link() {
-    let link = "https://paulatalim.github.io/pesquisa-social/";
-    link
+    const urlParams = new URLSearchParams(location.search);
+    let nome = urlParams.get('name');
+    let email = urlParams.get('e');
+
+    let link = `https://paulatalim.github.io/pesquisa-social/?name=${nome}&e=${email}`;
+
+    // Copia o link para a area de transferencia
+    navigator.clipboard.writeText(link);
+
+    let btn = document.getElementById("copiar-link");
+    btn.innerHTML = "link copiado !";
+    btn.style.border = "none";
+    btn.style.color = "white";
+    btn.style.background = "#EA0061";
+    
+}
+
+onload = () => {
+    document.getElementById("copiar-link").onclick = copiar_link;
 }
